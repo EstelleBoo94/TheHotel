46682ac44c8a35a25d00faa797d596511a7be58d
Estelle <estelleboo94@gmail.com>
2024-12-06 09:52:02 +0100

    Added ICrudOperations.

---
+++ CustomersFolder/CrudCustomer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheHotel.InterfaceFolder;
+
+namespace TheHotel.CustomersFolder
+{
+    public class CrudCustomer : ICrudOperations
+    {
+        public void Create()
+        {
+
+        }
+        public void Read()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+        //public int CustomerId { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public int SSN { get; set; }
+        //public int PhoneNumber { get; set; }
+        //public string Email { get; set; }
+        //public string? StreetAddress { get; set; }
+        //public int? PostalCode { get; set; }
+        //public string? City { get; set; }
+        //public string? Country { get; set; }
+    }
+}
